aaa8f69b6d7da4f9e515cc88fcafaf2a3f46d122
liuyang067 <liuyang2@stu.xju.edu.cn>
2026-07-01 16:25:45 +0800

    Add files via upload

---
+++ __config__.py
@@ -0,0 +1,9 @@
+# LIBRI3MIX_ROOT_PATH = '/mnt/data/libri_mix/Libri3Mix'
+# CHiME_ROOT_PATH = '/mnt/data/CHIME/processed_CHiME/'
+# LIBRICHiME_ROOT_PATH = '/mnt/data/CHIME/LibriCHiME5/'
+
+LIBRI3MIX_ROOT_PATH = '/home2/LiuY/Code/Web-GitHub/CHiME-7-baseline-main/baseline/dataset_loaders/dataset/LibriMix/Libri3Mix'
+CHiME_ROOT_PATH = '/home2/LiuY/Code/Web-GitHub/CHiME-7-baseline-main/baseline/dataset_loaders/dataset/CHiME_audio_segement'
+LIBRICHiME_ROOT_PATH = '/home2/LiuY/Code/Web-GitHub/CHiME-7-baseline-main/baseline/dataset_loaders/dataset/reverberant-LibriCHiME-5'
+
+API_KEY = 'insert_your_cometml_API_key_here'
